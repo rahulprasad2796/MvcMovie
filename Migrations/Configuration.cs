51e539ee2273372638b9518367fb5b80f29bbeb5
Rahul Prasad <rahul@arborgold.com>
2023-10-24 18:10:34 +0530

    Add project files.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,60 @@
+﻿using MvcMovie.Models;
+using System;
+using System.Data.Entity.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<MvcMovie.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MvcMovie.Models.MovieDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+
+            context.Movies.AddOrUpdate(i => i.Title,
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-1-11"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M,
+                    Rating = "PG"
+                },
+
+                 new Movie
+                 {
+                     Title = "Ghostbusters ",
+                     ReleaseDate = DateTime.Parse("1984-3-13"),
+                     Genre = "Comedy",
+                     Price = 8.99M,
+                     Rating = "UG"
+                 },
+
+                 new Movie
+                 {
+                     Title = "Ghostbusters 2",
+                     ReleaseDate = DateTime.Parse("1986-2-23"),
+                     Genre = "Comedy",
+                     Price = 9.99M,
+                     Rating = "UG"
+                 },
+
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M,
+                    Rating = "A"
+                }
+           );
+        }
+    }
+}
